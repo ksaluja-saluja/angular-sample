3bf669502de620e38aed13ba58a3b80f56c67171
ksaluja-saluja <saluja_kamal2000@yahoo.com>
2024-10-05 15:34:17 +1000

    switchmap and concatmap

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe, NgFor } from '@angular/common';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { map, of, Subscription } from 'rxjs';
+import { concatMap, delay, from, map, mergeMap, of, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-books',
@@ -16,11 +16,25 @@ export class BooksComponent implements OnInit, OnDestroy {
     { id: 3, name: 'book3', price: 80 },
   ]
 
+  // to create stream for switchmap and concat map
+  private values = ['book 1', 'book 2', 'book 3']
+
   books$ = of(this.books);
   booksMap$ = this.books$.pipe(
     map((data) => data.map((book) => `name - ${book.name}`))
   )
   booksSubscription: Subscription;
+
+
+  // switch map cancels all previous observable and just return the last one
+  booksSwitchMap$ = from(this.values).pipe(
+    switchMap((book) => of(book).pipe(delay(5000)))
+  )
+
+  // contact map doesn't cancel previous observable
+  booksConcatMap$ = from(this.values).pipe(
+    concatMap((book) => of(book).pipe(delay(2000)))
+  )
 
   ngOnInit(): void {
     // Another way to subscribe is Async Pipe in html
